8e1fdef28570efa9653316eae303ea7ecfe50a0f
Puneet Vashisht <puneetvashisht@gmail.com>
2022-09-28 12:15:15 +0530

    build price validation

--- my-first-app/src/AddEmployee.js
+++ my-first-app/src/AddEmployee.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react'
+import classes from './AddEmployee.module.css';
 
 export default function AddEmployee() {
 
     const [name, setName] = useState('')
     const [salary, setSalary] = useState('')
     const [message, setMessage] = useState('')
+    const [nameIsValid, setNameIsValid] = useState();
+    const [formIsValid, setFormIsValid] = useState(false);
+
+    const validateNameHandler = () => {
+        setNameIsValid(name.length > 3 );
+      };
 
     const handleNameChange = (event)=> {
         console.log(event.target.value)
         setName(event.target.value)
+        setFormIsValid(event.target.value.length > 3);
+        
     }
     const addEmployee = ()=> {
         console.log({name, salary})
@@ -35,16 +44,17 @@ export default function AddEmployee() {
         {message}
     </div>}
 
-    <div class="mb-3">
+    <div className={`mb-3 ${classes.control} ${nameIsValid === false ? classes.invalid : ''}`}>
+   
     <label forName="exampleFormControlInput1" className="form-label">Employee Name</label>
-    <input type="text" onChange={handleNameChange} value={name} className="form-control" id="exampleFormControlInput1" placeholder="Enter Name"/>
+    <input type="text"  onBlur={validateNameHandler} onChange={handleNameChange} value={name} className="form-control" id="exampleFormControlInput1" placeholder="Enter Name"/>
     </div>
     <div class="mb-3">
     <label forName="exampleFormControlInput2" className="form-label">Employee Salary</label>
     <input type="number" onChange={(event)=>setSalary(event.target.value)} value={salary} className="form-control" id="exampleFormControlInput2" placeholder="Enter Salary"/>
     </div>
     <div class="mb-3">
-    <button onClick={addEmployee} className="btn btn-primary">Add Employee</button>
+    <button onClick={addEmployee} className='btn btn-primary' disabled={!formIsValid}>Add Employee</button>
     </div>
 
     </>
--- shopping-cart/src/components/AddProduct.js
+++ shopping-cart/src/components/AddProduct.js
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
+import classes from './AddProduct.module.css'
 
 export default function AddProduct() {
+
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState('')
+    const [description, setDescription] = useState('')
+    const [nameIsValid, setNameIsValid] = useState();
+    const [descriptionIsValid, setDescriptionIsValid] = useState();
+    const [formIsValid, setFormIsValid] = useState(false);
+
+    const handleNameChange = (e) => {
+        setName(e.target.value)
+        setFormIsValid(e.target.value.length > 3);
+    }
+
+    const validateName = () => {
+        setNameIsValid(name.length > 3)
+    }
+
+    const handleDescriptionChange = (e) => {
+        setDescription(e.target.value)
+        setFormIsValid(e.target.value.length > 0);
+    }
+    const validateDescription = () => {
+        setDescriptionIsValid(description.length > 0)
+    }
+    const handlePriceChange = (e) => {
+        setPrice(e.target.value)
+    }
+
+    const addProduct = () => {
+        console.log({name, description, price});
+        //
+        fetch('http://localhost:8000/products', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({name, description, price})
+        })
+        .then(res=> {
+            if(res.status===201){
+                console.log('Successfully added to the server!')
+            }
+        })
+    }
   return (
-    <div>AddProduct</div>
+    <>
+    <div className={`mb-3 ${classes.control} ${nameIsValid === false ? classes.invalid : ''}`}>
+    <label htmlFor="exampleFormControlInput1" className="form-label">Product Name</label>
+    <input type="text" onBlur={validateName} onChange={handleNameChange} value={name} className="form-control" id="exampleFormControlInput1" placeholder="Enter Name"/>
+    </div>
+    <div className={`mb-3 ${classes.control} ${descriptionIsValid === false ? classes.invalid : ''}`}>
+    <label htmlFor="exampleFormControlInput1" className="form-label">Product Description</label>
+    <input type="text" onBlur={validateDescription} onChange={handleDescriptionChange} value={description} className="form-control" id="exampleFormControlInput1" placeholder="Enter Description"/>
+    </div>
+    <div className="mb-3">
+    <label htmlFor="exampleFormControlInput1" className="form-label">Price</label>
+    <input type="number" onChange={handlePriceChange} value={price} className="form-control" id="exampleFormControlInput1" placeholder="Enter Price"/>
+    </div>
+    <div className="mb-3">
+    <button onClick={addProduct} disabled={!formIsValid} className="btn btn-primary">Add Product</button>
+    </div>
+    </>
     
   )
 }
